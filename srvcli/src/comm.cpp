b85456d41744cad6a5c9ed2a269d1bce4b4574d1
jeong-wook <iyouhe@naver.com>
2023-09-04 21:34:35 +0900

    commit test

--- srvcli/src/comm.cpp
+++ srvcli/src/comm.cpp
@@ -1,5 +1,8 @@
 #include "unp.h"
 
+//winsock2 를 사용하기 위한 wrarpper 함수
+//윈도우 환경에서 소켓을 사용하려면 이 함수를  맨 처음에 실행을 해야 한다.
+//소켓 통신이 완전히 끝나면 WSACleanup()로 끝내야 한다.
 void STARTUP(){
     #ifdef WIN32
     WSADATA wsaData;
@@ -11,6 +14,7 @@ void STARTUP(){
     #endif
 }
 
+//에러가 발생시 에러메시지를 내고 강제 종료.
 void error_sys(const char * message){
     
     #ifdef WIN32
