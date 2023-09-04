8d8c742459d00e2bbc569f6a710982eb6562881a
jeong-wook <iyouhe@naver.com>
2023-09-04 22:51:05 +0900

    2nd test

--- srvcli/src/comm.cpp
+++ srvcli/src/comm.cpp
@@ -1,8 +1,5 @@
 #include "unp.h"
 
-//winsock2 를 사용하기 위한 wrarpper 함수
-//윈도우 환경에서 소켓을 사용하려면 이 함수를  맨 처음에 실행을 해야 한다.
-//소켓 통신이 완전히 끝나면 WSACleanup()로 끝내야 한다.
 void STARTUP(){
     #ifdef WIN32
     WSADATA wsaData;
